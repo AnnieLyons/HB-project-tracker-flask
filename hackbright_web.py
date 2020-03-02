486e84d0ed195e696d29fe04faa1c96a657e4623
Susanna Lyons <susannarlyons@gmail.com>
2020-03-02 15:02:38 -0800

    added project and homepage routes to hackbright_web.py

--- hackbright_web.py
+++ hackbright_web.py
@@ -7,6 +7,13 @@ import hackbright
 app = Flask(__name__)
 
 
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
 @app.route("/student")
 def get_student():
     """Show information about a student."""
@@ -22,11 +29,6 @@ def get_student():
 
     return html
 
-@app.route("/student-search")
-def get_student_form():
-    """Show form for searching for a student."""
-
-    return render_template("student_search.html")
 
 @app.route("/display_form")
 def show_student_form():
@@ -49,6 +51,35 @@ def make_new_student():
                             first_name=first_name,
                             last_name=last_name,
                             github=github)
+
+
+@app.route("/project")
+def get_project():
+    """Show information about a project."""
+
+    title = request.args.get("title")
+
+    title, description, max_grade = hackbright.get_project_by_title(title)
+
+    grades = hackbright.get_grades_by_title(title)
+
+    return render_template("project_info.html",
+                           title=title,
+                           description=description,
+                           max_grade=max_grade,
+                           grades=grades)
+
+
+@app.route("/")
+def homepage():
+    """Show listing of students and projects."""
+
+    students = hackbright.get_all_students()
+    projects = hackbright.get_all_projects()
+
+    return render_template("homepage.html",
+                           students=students,
+                           projects=projects)
 
 
 if __name__ == "__main__":
